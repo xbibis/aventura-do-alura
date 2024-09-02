c70be155680a60696bc14eee95ea27232e577575
bibis <167935451+xbibis@users.noreply.github.com>
2024-09-02 09:22:52 -0300

    projeto final

---
+++ script.js
@@ -0,0 +1,11 @@
+const avança = document.querySelectorAll('.btn-proximo');
+
+avança.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
